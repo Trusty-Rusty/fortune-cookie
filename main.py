dbfea87d32096c54fb9312eb814db66bf8742bbd
Andrew Russell <andy.russell.code@gmail.com>
2017-03-23 11:27:47 -0500

    Added fortune, lucky #, and repeat button to main.py

--- main.py
+++ main.py
@@ -15,11 +15,30 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def getRandomFortune():
+    fortunes = ["You will die.", "You will not die.", "You will eat a snack."]#list of possible fortune_sentence
+    daily_fortune = fortunes[random.randrange(len(fortunes))]
+    return daily_fortune
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        header = "<h1>Fortune Cookie</h1>"
+
+        fortune = getRandomFortune()
+        fortune_sentence = "Your fortune: " + fortune
+        fortune_paragraph = "<p>" + fortune_sentence + "</p>"
+
+        lucky_number = random.randrange(1, 100)
+        number_sentence = ("Your lucky number: " + str(lucky_number))
+        number_paragraph = "<p>" + number_sentence + "</p>"
+        another_cookie_button = "<a href='.'><button>Another Cookie?</button></a>"
+
+        content = header + fortune_paragraph + number_paragraph + another_cookie_button
+        self.response.write(content)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
-], debug=True)
+    ], debug=True)
